b4c852f09127b8a238b6a3124c4e9b246df90627
Abhinav <66483588+abhikrjh@users.noreply.github.com>
2022-03-26 23:39:11 +0530

    Add files via upload
    function to find the longest palindrome in a given string

--- ProblemSolving/LongestPalindrome.java
+++ ProblemSolving/LongestPalindrome.java
@@ -1,92 +1,54 @@
-package problemsolving;
+package com.corejava;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Map.Entry;
+//Write a function to find out longest palindrome in a given string?
 
 public class LongestPalindrome {
-	
-	public static String longestPalindrome(String input) {
-		String longestPalindrome = "";
-		Map<String, Integer> map = new HashMap<String, Integer>();
-		String temp = "";int j=0;
-		label1:for (int i = j; i < input.length();) {
-			char c = input.charAt(i);
-			temp = temp + c;
 
-			if (isPalindrome(temp)) {
-				map.put(temp, temp.length());
-			}
+    public String longestPalindrome(String str) {
+        String longest = "";
+        char[] charArray = str.toCharArray();
+        longest = longest(str, charArray);
+        return longest;
+    }
 
-			if (null !=  map.get(temp) && (map.get(temp) > longestPalindrome.length())) {
-				longestPalindrome = temp;
-			}
-			
-			if (i == input.length() - 1) {
-				temp = "";
-				j = j+1;
-				i = j;
-				continue label1;
-			}
-			
-			i++;
-		}
-		return longestPalindrome;
-	}
+    public String longest(String str, char[] charArray) {
+        String longest="";
+        int currentFirstIndex = 0;
+        int currentLastIndex = 0;
+        boolean matchStarts = false;
+        for (int i = 0; i < charArray.length - 1; i++) {
+            if(longest.isEmpty()){
+                longest = str.substring(currentFirstIndex, currentLastIndex+1);
+            }else{
+                String current = str.substring(currentFirstIndex, currentLastIndex+1);
+                if(longest.length()<current.length()){
+                    longest = current;
+                }
+            }
+            int tempI = i;
+            for (int j = charArray.length - 1; j > i; j--) {
+                if (charArray[tempI] == charArray[j]) {
+                    if(!matchStarts){
+                        currentFirstIndex = tempI;
+                        currentLastIndex = j;
+                    }
+                    tempI++;
+                    matchStarts = true;
+                } else {
+                    currentFirstIndex = 0;
+                    currentLastIndex = 0;
+                    matchStarts = false;
+                }
 
-	public static String longestPalindrome1(String input) {
-		String longestPalindrome = "";
-		Map<String, Integer> map = new LinkedHashMap<String, Integer>();
-		String str = "";
-		int j = 0;
-		label: for (int i = j; i < input.length(); i++) {
+            }
+        }
+        return longest;
+    }
 
-			char c = input.charAt(i);
-			str = str + c + "";
-			boolean isPalindrome = isPalindrome(str);
-			if (isPalindrome) {
-				map.put(str, str.length());
-			}
-			if (i == input.length() - 1) {
-				j = j + 1;
-				i = j;
-				str = "";
-				continue label;
-			}
-		}
-
-		Map.Entry<String, Integer> entryWithMaxValue = null;
-		for (Entry<String, Integer> currentEntry : map.entrySet()) {
-
-			if (null == entryWithMaxValue || currentEntry.getValue().compareTo(entryWithMaxValue.getValue()) > 0) {
-				entryWithMaxValue = currentEntry;
-			}
-
-		}
-		if (null != entryWithMaxValue) {
-			longestPalindrome = entryWithMaxValue.getKey();
-		}
-		return longestPalindrome;
-	}
-
-	public static boolean isPalindrome(String str) {
-		char[] charArr = str.toCharArray();
-		for (int i = 0; i < charArr.length / 2; i++) {
-			if (charArr[i] != charArr[charArr.length - (i + 1)]) {
-				return false;
-			}
-		}
-
-		return true;
-	}
-
-	public static void main(String[] args) {
-		//String input = "aaaabbaa";
-		
-		String input ="rfkqyuqfjkxy";
-
-		System.out.println(longestPalindrome(input));
-	}
+    public static void main(String[] args) {
+        LongestPalindrome lp = new LongestPalindrome();
+        String str = lp.longestPalindrome("abcmalayalamdc");
+        System.out.println(str + " is the longest palindrome ");
+    }
 
 }
